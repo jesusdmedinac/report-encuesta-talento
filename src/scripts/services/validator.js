d91fcef875893cfac4892e32bcbdd010f7e96857
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2025-09-18 03:49:16 -0400

    Validator with ajv-formats

--- src/scripts/services/validator.js
+++ src/scripts/services/validator.js
@@ -1,4 +1,5 @@
 import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -8,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const ajv = new Ajv({ allErrors: true });
+addFormats(ajv);
 
 /**
  * Valida un objeto de datos contra un esquema JSON especificado.
