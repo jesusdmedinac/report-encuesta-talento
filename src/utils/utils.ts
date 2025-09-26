2e3457d7cd506acd3c68520b9cfc7626c94687f1
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2025-09-26 02:56:08 -0400

    Brecha digital with percentiles

---
+++ src/utils/utils.ts
@@ -0,0 +1,16 @@
+import scales from "../scripts/baremos.json"
+
+export type ScaleDecile = {
+  puntaje: number;
+  nivel: string;
+  desde: number;
+  hasta: number;
+}
+
+export const scoreToScaleDecile: (score: number, dimension: string) => ScaleDecile | undefined = (score: number, dimension: string) => {   
+  const generalDeciles = scales.deciles.general as {
+    [key: string]: ScaleDecile[]
+  }
+  const decile = generalDeciles[dimension].find((decile) => score >= decile.desde && score <= decile.hasta)
+  return decile
+}
